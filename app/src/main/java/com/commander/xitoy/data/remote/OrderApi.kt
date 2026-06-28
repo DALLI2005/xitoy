d293c7136ac8a3f3ddfde801952c6c54c21270ce
ELIBOYEV D. D. <76607009+DALLI2005@users.noreply.github.com>
2026-06-28 12:30:52 +0500

    auto: Claude o'zgartirdi

--- app/src/main/java/com/commander/xitoy/data/remote/OrderApi.kt
+++ app/src/main/java/com/commander/xitoy/data/remote/OrderApi.kt
@@ -1,11 +1,14 @@
 package com.commander.xitoy.data.remote
 
+import com.commander.xitoy.domain.model.Product
 import okhttp3.MultipartBody
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 data class OrderItemDetail(
